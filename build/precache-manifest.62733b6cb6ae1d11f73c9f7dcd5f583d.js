98ad00789fce6cb12613dbb4931751b6f2b17b73
Yogesh Panchal <yogesh.panchal@infoedge.com>
2021-05-07 14:09:14 +0530

    second commit

---
+++ build/precache-manifest.62733b6cb6ae1d11f73c9f7dcd5f583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ebf65ddaad147ce1a53f65854e5ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c477637a370554a7e06",
+    "url": "/static/css/main.0527dc8c.chunk.css"
+  },
+  {
+    "revision": "06586306daca51da03e7",
+    "url": "/static/js/2.99a1d723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99a1d723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c477637a370554a7e06",
+    "url": "/static/js/main.ded5e249.chunk.js"
+  },
+  {
+    "revision": "678edcf564e56468679b",
+    "url": "/static/js/runtime-main.1e6e7623.js"
+  }
+]);
